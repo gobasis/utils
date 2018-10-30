4cdb7d74e93414e56ce3b9ae6d815754a3cab0f8
architect.bian <greatarchitect@163.com>
2018-10-30 17:58:12 +0800

    add RunEnv util

---
+++ RunEnv.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"time"
+	"math/rand"
+	"runtime"
+)
+
+type runEnvType struct {}
+
+var RunEnv runEnvType
+
+/*
+Description:
+setup run environment
+ * Author: architect.bian
+ * Date: 2018/08/06 16:16
+ */
+func (_ runEnvType) Setup() {
+	rand.Seed(time.Now().UnixNano())
+	// Setting a higher number here allows more disk I/O calls to be scheduled, hence considerably
+	// improving throughput. The extra CPU overhead is almost negligible in comparison.
+	runtime.GOMAXPROCS(128)
+}
